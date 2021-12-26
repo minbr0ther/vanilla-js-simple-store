326eade0a818177e018a2f2e73955a22f54ebf8f
minbr0ther <minbr0ther@naver.com>
2021-12-26 23:42:19 +0900

    feat: make observer functionalization 앞서 작성한 코드를 재사용하기 위해서 observe와 observable 함수로 만들어야 한다.

---
+++ 01-pubsub/example/observer.js
@@ -0,0 +1,45 @@
+let currentObserver = null;
+
+const observe = (fn) => {
+  currentObserver = fn;
+  fn();
+  currentObserver = null;
+};
+
+const observable = (obj) => {
+  Object.keys(obj).forEach((key) => {
+    let _value = obj[key];
+
+    // 이제 여러 개의 observer를 만들어서 관리해보자.
+    const observers = new Set();
+
+    Object.defineProperty(obj, key, {
+      get() {
+        // state의 property가 사용될 때(=get 메소드가 실행될 때)
+        // currentObserver를 observers에 등록한다.
+        if (currentObserver) observers.add(currentObserver);
+
+        return _value;
+      },
+
+      set(value) {
+        _value = value;
+
+        // state의 property가 변경될 때(=set 메소드가 실행될 때)
+        // observers에 등록된 모든 observer를 실행한다.
+        observers.forEach((fn) => fn());
+      },
+    });
+  });
+  return obj;
+};
+
+const 상태 = observable({ a: 10, b: 20 });
+observe(() => console.log(`a = ${상태.a}`));
+observe(() => console.log(`b = ${상태.b}`));
+observe(() => console.log(`a + b = ${상태.a} + ${상태.b}`));
+observe(() => console.log(`a * b = ${상태.a} + ${상태.b}`));
+observe(() => console.log(`a - b = ${상태.a} + ${상태.b}`));
+
+상태.a = 100;
+상태.b = 200;
