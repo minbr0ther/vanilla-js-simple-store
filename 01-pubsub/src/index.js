da0eb82fc659eccb575d85a902e0f1385e6bbfe7
minbr0ther <minbr0ther@naver.com>
2021-12-26 22:48:30 +0900

    feat: add store Store는 여러 개의 컴포넌트에서 사용될 수 있다.. Store가 변경될 때, Store가 사용되고 있는 Component도 변경되어야 한다.

--- 01-pubsub/src/index.js
+++ 01-pubsub/src/index.js
@@ -1,1 +1,16 @@
-console.log('hi');
+// Store를 생성한다.
+const store = new Store({ a: 10, b: 20 });
+
+// 컴포넌트를 생성한다.
+const component1 = new Component({ subscribe: [store] });
+const component2 = new Component({ subscribe: [store] });
+
+// 컴포넌트가 store을 구독한다.
+component1.subscribe(store); // a + b = ${store.state.a + store.state.b}
+component2.subscribe(store); // a + b = ${store.state.a + store.state.b}
+
+// store의 state를 변경한다.
+store.setState({ a: 100, b: 200 });
+
+// store가 변경되었음을 알린다.
+store.notify();
