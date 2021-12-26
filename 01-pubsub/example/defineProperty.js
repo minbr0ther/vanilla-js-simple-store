823de469d037c79d50325514216e3b49f781955f
minbr0ther <minbr0ther@naver.com>
2021-12-26 23:35:44 +0900

    feat: manage various observer 이 코드에서 핵심은, 함수가 실행될 때 currentObsever가 실행중인 함수를 참조하도록 만드는 것 이다. state의 property가 사용될 때(=get 메소드가 실행될 때) currentObserver를 observers에 등록한다. state의 property가 변경될 때(=set 메소드가 실행될 때) observers에 등록된 모든 observer를 실행한다.

--- 01-pubsub/example/defineProperty.js
+++ 01-pubsub/example/defineProperty.js
@@ -1,25 +1,51 @@
+let currentObserver = null;
+
 const state = {
   a: 10,
   b: 20,
 };
 
 const stateKeys = Object.keys(state); // ['a', 'b']
-const observer = () => console.log(`a + b = ${state.a + state.b}`);
 
 for (const key of stateKeys) {
   let _value = state[key]; //'10', '20'
+
+  // 이제 여러 개의 observer를 만들어서 관리해보자.
+  const observers = new Set();
+
   Object.defineProperty(state, key, {
     get() {
+      // state의 property가 사용될 때(=get 메소드가 실행될 때)
+      // currentObserver를 observers에 등록한다.
+      if (currentObserver) observers.add(currentObserver);
+
       return _value;
     },
     set(value) {
       _value = value;
-      observer();
+
+      // state의 property가 변경될 때(=set 메소드가 실행될 때)
+      // observers에 등록된 모든 observer를 실행한다.
+      observers.forEach((observer) => observer());
     },
   });
 }
 
-observer(); // a + b = 30
+const 덧셈_계산기 = () => {
+  currentObserver = 덧셈_계산기;
+  console.log(`a + b = ${state.a + state.b}`);
+};
 
-state.a = 100; // a + b = 120
-state.b = 200; // a + b = 300
+const 뺄셈_계산기 = () => {
+  currentObserver = 뺄셈_계산기;
+  console.log(`a - b = ${state.a - state.b}`);
+};
+
+덧셈_계산기(); // a + b = 30
+state.a = 100; // a + b = 120, a - b = 80
+
+뺄셈_계산기(); // a - b = 80
+state.b = 200;
+
+state.a = 1;
+state.b = 2;
